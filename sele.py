14156526f79c762b09ba6a2a27abd2e84c67e98a
loserlovey <chenshaoyi_2000@hotmail.com>
2018-01-03 15:28:10 +0800

    update to add new node

--- sele.py
+++ sele.py
@@ -13,6 +13,7 @@ def process_wahuasuan():
     my_password = myconfig['wahuasuan_pwd']
     browser = webdriver.Firefox()
     browser.get(url)
+    sleep(5)
     browser.find_element_by_id(id_field_name).clear()
     browser.find_element_by_id(id_field_name).send_keys(my_id)
     browser.find_element_by_id(passwd_field_name).clear()
@@ -33,7 +34,7 @@ def process_jd():
     btn_field_id = 'loginsubmit'
     browser = webdriver.Firefox()
     browser.get(passporturl)
-    
+    sleep(5)
     try:
         browser.find_element_by_link_text(u'账户登录').click()
         browser.find_element_by_id(user_field_id).clear()
@@ -41,6 +42,7 @@ def process_jd():
         browser.find_element_by_id(password_field_id).clear()
         browser.find_element_by_id(password_field_id).send_keys(jd_passwd)
         browser.find_element_by_id(btn_field_id).click()
+        sleep(5)
         browser.find_element_by_class_name("icon-sign").click()
     except:
         pass
@@ -48,7 +50,7 @@ def process_jd():
     sleep(10)
     passporturl = "https://passport.jd.com/new/login.aspx?ReturnUrl=http%3A%2F%2Fvip.jr.jd.com%2F"
     browser.get(passporturl)
-
+    sleep(5)
     try:
         browser.find_element_by_link_text(u'账户登录').click()
         browser.find_element_by_id(user_field_id).clear()
@@ -56,6 +58,7 @@ def process_jd():
         browser.find_element_by_id(password_field_id).clear()
         browser.find_element_by_id(password_field_id).send_keys(jd_passwd)
         browser.find_element_by_id(btn_field_id).click()
+        sleep(5)
         browser.find_element_by_class_name("qian-text").click()
     except:
         pass
@@ -73,6 +76,36 @@ def process_jd():
 
     print(u'jd finished...')
 
+def process_91wenwen():
+    url = 'https://91wenwen.net/user/login'
+    wenwen_id = myconfig['91wenwen_id']
+    wenwen_pwd = myconfig['91wenwen_pwd']
+    user_field_id = 'login_email'
+    password_field_id = 'login_password'
+    btn_field_class_name = 'login-btn'
+    browser = webdriver.Firefox()
+    browser.get(url)
+    sleep(5)
+
+    try:
+        browser.find_element_by_id(user_field_id).clear()
+        browser.find_element_by_id(user_field_id).send_keys(wenwen_id)
+        browser.find_element_by_id(password_field_id).clear()
+        browser.find_element_by_id(password_field_id).send_keys(wenwen_pwd)
+        browser.find_element_by_class_name(btn_field_class_name).click()
+        sleep(5)
+        browser.get('http://91wenwen.net/vote/index')
+        browser.find_element_by_link_text(u'去投票').click()
+        browser.find_element_by_id('answer_number_1').click()
+        browser.find_element_by_class_name('btn.submit').click()
+    except:
+        pass
+
+    browser.quit()
+    print(u'91wenwen finished...')
+
+
 if __name__ == '__main__':
     process_wahuasuan()
     process_jd()
+    process_91wenwen()
